7aada9ddd43bd08c3475241095b233cd3e36c2ed
anton2920 <anton2920@gmail.com>
2022-12-14 13:34:32 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,184 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/knownhosts"
+)
+
+/* TODO:
+ * 1. Better prompt handling.
+ * 2. Infinite execution.
+ * 3. Proper signal handling.
+ */
+
+func printUsageAndExit() {
+	fmt.Fprintln(os.Stderr, "usage: gossh user@server")
+	os.Exit(1)
+}
+
+func main() {
+	var host, user, pwd string
+
+	if len(os.Args) != 2 {
+		printUsageAndExit()
+	}
+
+	args := strings.Split(os.Args[1], "@")
+	if len(args) == 1 {
+		user = "glenda"
+		host = args[0]
+	} else if len(args) == 2 {
+		user = args[0]
+		host = args[1]
+	} else {
+		printUsageAndExit()
+	}
+	if strings.Index(host, ":") == -1 {
+		host += ":22"
+	}
+
+	fmt.Print("Password: ")
+	_, err := fmt.Scanf("%s", &pwd)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to scan password: ", err)
+		return
+	}
+
+	var hostkeyCallback ssh.HostKeyCallback
+	hostkeyCallback, err = knownhosts.New("~/.ssh/known_hosts")
+	if err != nil {
+		/* NOTE: fallback to ignoring everything as we don't care :) */
+		fmt.Fprintln(os.Stderr, "WARNING: falling back to 'ssh.InsecureIgnoreHostKey()'")
+		hostkeyCallback = ssh.InsecureIgnoreHostKey()
+	}
+
+	conf := &ssh.ClientConfig{
+		User:            user,
+		HostKeyCallback: hostkeyCallback,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(pwd),
+		},
+	}
+
+	var conn *ssh.Client
+
+	conn, err = ssh.Dial("tcp", host, conf)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to connect to the server: ", err)
+		return
+	}
+	defer conn.Close()
+
+	var session *ssh.Session
+	var stdin io.WriteCloser
+	var stdout, stderr io.Reader
+
+	session, err = conn.NewSession()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to open a session: ", err)
+		return
+	}
+	defer session.Close()
+
+	stdin, err = session.StdinPipe()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to get stdin pipe: ", err)
+		return
+	}
+
+	stdout, err = session.StdoutPipe()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to get stdout pipe: ", err)
+		return
+	}
+
+	stderr, err = session.StderrPipe()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to get stderr pipe: ", err)
+		return
+	}
+
+	scannerFunc := func(reader io.Reader, sendChan chan<- string) {
+		scanner := bufio.NewScanner(reader)
+
+		for scanner.Scan() {
+			sendChan <- scanner.Text()
+		}
+		if reader != stderr {
+			close(sendChan)
+		}
+	}
+
+	inChan := make(chan string)
+	outChan := make(chan string, 128)
+
+	go scannerFunc(os.Stdin, inChan)
+	go scannerFunc(stdout, outChan)
+	go scannerFunc(stderr, outChan)
+
+	promptChan := make(chan struct{}, 1)
+	session.Shell()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	/* NOTE: main loop */
+	const prompt = "% "
+
+mainFor:
+	for {
+		select {
+		case signal := <-sigChan:
+			switch signal {
+			case syscall.SIGINT:
+				session.Signal(ssh.SIGINT)
+			case syscall.SIGTERM:
+				session.Signal(ssh.SIGTERM)
+			case syscall.SIGQUIT:
+				session.Signal(ssh.SIGQUIT)
+			}
+		case outstr, ok := <-outChan:
+			if !ok {
+				break mainFor
+			}
+
+			fmt.Println(outstr)
+
+			/* NOTE: draining output channel */
+		outputFor:
+			for {
+				select {
+				case outstr := <-outChan:
+					fmt.Println(outstr)
+				default:
+					break outputFor
+				}
+			}
+
+			if len(promptChan) == 0 {
+				promptChan <- struct{}{}
+			}
+		case instr, ok := <-inChan:
+			if !ok {
+				break mainFor
+			}
+			_, err := stdin.Write([]byte(instr + "\n"))
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Failed to send a command: ", err)
+				return
+			}
+		case <-promptChan:
+			fmt.Print(prompt)
+		}
+	}
+
+	fmt.Println()
+}
